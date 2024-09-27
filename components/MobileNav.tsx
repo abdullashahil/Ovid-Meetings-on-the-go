4dff65b55dd3e01150433a2e6643a145b3a2ff74
Abdulla Shahil <abdullashahil0077@gmail.com>
2024-09-27 11:58:37 +0530

    Clerk Authentication - Sign in and Sign Up

---
+++ components/MobileNav.tsx
@@ -0,0 +1,69 @@
+'use client'
+import React from 'react'
+
+import { sideBarLinks } from '@/constants'
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet"
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
+
+const MobileNav = () => {
+    const pathname = usePathname();
+
+    return (
+        <section className='w-full max-w-[264px] '>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Image src='/icons/hamburger.svg' width={36} height={36} alt='Hamburger icon'
+                        className='cursor-pointer sm:hidden' />
+                </SheetTrigger>
+                <SheetContent side='left' className='border-none bg-color-1'>
+                    <Link href='/' className='flex items-center  '>
+                        <Image alt='logo' src='/icons/logo.svg'
+                            width={32}
+                            height={32}
+                            className='max-sm:size-10'
+                        /> 
+                        <p className='text-[26px] font-extrabold text-white '>Ovid</p>
+                    </Link>
+
+                    <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+                        <SheetClose asChild>
+                            <section className=" flex h-full flex-col gap-6 pt-16 text-white">
+                                {sideBarLinks.map((item) => {
+                                    const isActive = pathname === item.route;
+
+                                    return (
+                                        <SheetClose asChild key={item.route}>
+                                            <Link
+                                                href={item.route}
+                                                key={item.label}
+                                                className={cn(
+                                                    'flex gap-4 items-center p-4 rounded-lg w-full max-w-60',
+                                                    {
+                                                        'bg-color-2': isActive,
+                                                    }
+                                                )}
+                                            >
+                                                <Image
+                                                    src={item.imgURL}
+                                                    alt={item.label}
+                                                    width={20}
+                                                    height={20}
+                                                />
+                                                <p className="font-semibold">{item.label}</p>
+                                            </Link>
+                                        </SheetClose>
+                                    );
+                                })}
+                            </section>
+                        </SheetClose>
+                    </div>
+                </SheetContent>
+            </Sheet>
+        </section>
+    )
+}
+
+export default MobileNav
