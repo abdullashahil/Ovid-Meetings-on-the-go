4213e3566c01c08a500e7e735e476c90c6a9b2d3
Abdulla Shahil <abdullashahil0077@gmail.com>
2024-10-25 14:57:30 +0530

    Frontend UI designing

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,5 +1,5 @@
 import Sidebar from '@/components/Sidebar'
-import Navbar from '@/components/navbar'
+import Navbar from '@/components/Navbar'
 import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
 
