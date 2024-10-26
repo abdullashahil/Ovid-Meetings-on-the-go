9039fe7321245ca91e885fe49f3b29d49579b332
Abdulla Shahil <abdullashahil0077@gmail.com>
2024-10-26 14:22:09 +0530

    Deployment

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,5 +1,4 @@
 'use client';
-// @ts-ignore
 
 import { Call } from '@stream-io/video-client/dist/src/gen/video/sfu/models/models';
 import { CallRecording } from '@stream-io/node-sdk';
@@ -45,55 +44,59 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
   useEffect(() => {
     const fetchRecordings = async () => {
       const callData = await Promise.all(
+        // @ts-expect-error
         callRecordings?.map((meeting) => meeting.queryRecordings()) ?? [],
-      );
-
-      const recordings = callData
+        );
+        
+        const recordings = callData
         .filter((call) => call.recordings.length > 0)
         .flatMap((call) => call.recordings);
-
-      setRecordings(recordings);
-    };
-
-    if (type === 'recordings') {
-      fetchRecordings();
-    }
-  }, [type, callRecordings]);
-
-  if (isLoading) return <Loader />;
-
-  const calls = getCalls();
-  const noCallsMessage = getNoCallsMessage();
-
-  return (
-    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+        
+        setRecordings(recordings);
+      };
+      
+      if (type === 'recordings') {
+        fetchRecordings();
+      }
+    }, [type, callRecordings]);
+    
+    if (isLoading) return <Loader />;
+    
+    const calls = getCalls();
+    const noCallsMessage = getNoCallsMessage();
+    
+    return (
+      <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
       {calls && calls.length > 0 ? (
         calls.map((meeting: Call | CallRecording) => (
           <MeetingCard
-            key={(meeting as Call).id}
-            icon={
-              type === 'ended'
-                ? '/icons/previous.svg'
-                : type === 'upcoming'
-                  ? '/icons/upcoming.svg'
-                  : '/icons/recordings.svg'
-            }
-            title={
+          key={(meeting as Call).id}
+          icon={
+            type === 'ended'
+            ? '/icons/previous.svg'
+            : type === 'upcoming'
+            ? '/icons/upcoming.svg'
+            : '/icons/recordings.svg'
+          }
+          title={
+              // @ts-expect-error
               (meeting as Call).state?.custom?.description ||
               (meeting as CallRecording).filename?.substring(0, 20) ||
               'No Description'
             }
             date={
+              // @ts-expect-error
               (meeting as Call).state?.startsAt?.toLocaleString() ||
               (meeting as CallRecording).start_time?.toLocaleString()
             }
             isPreviousMeeting={type === 'ended'}
             link={
               type === 'recordings'
-                ? (meeting as CallRecording).url
-                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${(meeting as Call).id}`
+              ? (meeting as CallRecording).url
+              : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${(meeting as Call).id}`
             }
             buttonIcon1={type === 'recordings' ? '/icons/play.svg' : undefined}
+
             buttonText={type === 'recordings' ? 'Play' : 'Start'}
             handleClick={
               type === 'recordings'
